c7cbd12e6812bc309c35e20b69d0726047bb6ca2
soominnnn <marisol03@naver.com>
2023-04-12 23:25:51 +0900

    Gambling

--- app/src/main/java/com/example/mobileprogramming20230406/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming20230406/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView ImageViewP1Num1,ImageViewP1Num2,ImageViewP2Num1, ImageViewP2Num2;
     ImageButton imageButton;
+
+    TextView winner, percentageP1, percentageP2 ;
 
     int[] clover = {R.drawable.clover1,R.drawable.clover2,R.drawable.clover3,
             R.drawable.clover4,R.drawable.clover5,R.drawable.clover6,R.drawable.clover7,
@@ -33,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.daia8,R.drawable.daia9,R.drawable.daia10,R.drawable.daia11,
             R.drawable.daia12,R.drawable.daia13};
 
+    int WinP1 = 0;
+    int WinP2 = 0;
+    int Count = 0;
+
+    long Sum_string = 0;
+    long Sum_string2 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,21 +55,44 @@ public class MainActivity extends AppCompatActivity {
 
         imageButton = findViewById(R.id.imageButton);
 
+        winner = findViewById(R.id.winnerPlayer);
+        percentageP1 = findViewById(R.id.percentageP1);
+        percentageP2 = findViewById(R.id.percentageP2);
+
         ImageView[] imageViews = {ImageViewP1Num1,ImageViewP1Num2,
                                   ImageViewP2Num1,ImageViewP2Num2 };
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Count++;
                 Random random = new Random();
-                int randomP1 = random.nextInt(14);
-                int randomP2 = random.nextInt(14);
-                int randomP12 = random.nextInt(14);
-                int randomP22 = random.nextInt(14);
+                int randomP1 = random.nextInt(13);
+                int randomP2 = random.nextInt(13);
+                int randomP12 = random.nextInt(13);
+                int randomP22 = random.nextInt(13);
                 imageViews[0].setImageResource(heart[randomP1]);
                 imageViews[1].setImageResource(clover[randomP12]);
                 imageViews[2].setImageResource(spade[randomP2]);
                 imageViews[3].setImageResource(daia[randomP22]);
+
+                if(randomP1 + randomP12 > randomP2 + randomP22){
+                    winner.setText("Player1 WIN!");
+                    WinP1++;
+                }
+                else if(randomP1 + randomP12 == randomP2 + randomP22){
+                    winner.setText("무승부!");
+                }
+                else{
+                    winner.setText("Player2 WIN!");
+                    WinP2++;
+                }
+                double Sum = (((float) WinP1 / Count) * 100);
+                Sum_string = Math.round(Sum);
+                percentageP1.setText(String.valueOf(Sum_string) + "%");
+                double Sum2 = (((float) WinP2 / Count) * 100);
+                Sum_string2 = Math.round(Sum2);
+                percentageP2.setText(String.valueOf(Sum_string2) + "%");
             }
         });
     }
